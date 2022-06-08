5aed892a8ddbc4d3c5e89e3460d920ae99df2ef5
Cory Pilat <56729741+cpilat97@users.noreply.github.com>
2022-06-08 12:13:26 -0400

    initial upload with function

---
+++ R/fix_filepath.R
@@ -0,0 +1,25 @@
+#' Fix a copied windows filepath
+#'
+#' @return Character vector that has a cleaned, modified windows file path replacing
+#' all forward slashes with a single backslash so it can be recopied and pasted in
+#' windows file explorer
+#'
+#' @export
+#'
+#' @examples \dontrun{
+#' clipr::write_clip("C:\\Users\\Test", allow_non_interactive = TRUE)
+#' fix_filepath()
+#'}
+fix_filepath <- function() {
+  copied_filepath <- clipr::read_clip()
+
+  if(grepl("\\\\", copied_filepath)) {
+    fix_the_clip <- gsub("\\\\", "/", copied_filepath)
+
+    return(fix_the_clip)
+  } else {
+
+    warning("Did you copy a windows file path? No double-forward slashes detected")
+
+  }
+}
