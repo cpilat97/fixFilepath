de498d9f4597d1bf4b0fe6f9a9573485499e43dc
Cory Pilat <56729741+cpilat97@users.noreply.github.com>
2022-06-19 10:57:31 -0400

    added in RStudio addin for the open_path func call. amended some of the docs for better readability

--- R/open_path.R
+++ R/open_path.R
@@ -1,11 +1,11 @@
-#' open_path. Open path will read a url, file path, directory label, etc.. from
+#' open_path.
+#'
+#' @return Open path will read a url, file path, directory label, etc.. from
 #' your clipboard and open that for you. Just note, there's not much safety net
 #' regarding checking for a valid url, file path, etc... because of the
 #' variability that may be associated with them. The function will throw an error
 #' in two cases: not used in an interactive session (ie trying to call from
 #' command line) or if the path cannot be opened.
-#'
-#' @return
 #' @export
 #'
 #' @examples \dontrun{
