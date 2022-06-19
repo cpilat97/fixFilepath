e342ac15c2feae3eec55bcec2afe41804ea180f3
Cory Pilat <56729741+cpilat97@users.noreply.github.com>
2022-06-18 20:33:50 -0400

    added in open_path func

---
+++ R/open_path.R
@@ -0,0 +1,24 @@
+#' open_path. Open path will read a url, file path, directory label, etc.. from
+#' your clipboard and open that for you. Just note, there's not much safety net
+#' regarding checking for a valid url, file path, etc... because of the
+#' variability that may be associated with them. The function will throw an error
+#' in two cases: not used in an interactive session (ie trying to call from
+#' command line) or if the path cannot be opened.
+#'
+#' @return
+#' @export
+#'
+#' @examples \dontrun{
+#' clipr::write_clip("C:\\Users\\Test")
+#' open_path()
+#'
+#' clipr::write_clip("https://github.com/cpilat97/fixFilepath")
+#' open_path()
+#'}
+open_path <- function() {
+  if (!interactive()) {
+    stop("This can only be used in an interactive session")
+  } else {
+    utils::browseURL(clipr::read_clip())
+  }
+}
